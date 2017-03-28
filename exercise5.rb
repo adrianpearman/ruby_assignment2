492ca9c22c6e6cc10f7f5ffa355076489ccee270
Adrian Pearman <adrianpearman12@gmail.com>
2017-03-28 13:30:18 -0400

    finish exercise 5 & 6

--- exercise5.rb
+++ exercise5.rb
@@ -1,1 +1,9 @@
+names = ["Bob", "Shirly", "Sue", "Andy"]
+names.each do |name|
+puts "hello #{name.reverse}"
+end
 
+
+puts "What is your name?"
+greeting_backwards = gets.chomp.to_s.reverse
+puts "Hello #{greeting_backwards}!"
--- exercise6.rb
+++ exercise6.rb
@@ -1,1 +1,4 @@
-]
+puts "What is the Temperature in Fahrenheit"
+temperature = gets.to_f
+convert = ((temperature - 32) * 5)/9
+puts "The Temperatue in Celscius is #{convert.to_s}"
