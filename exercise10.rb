5eeef43a7e243e58060addf95c54110f032fef42
Adrian Pearman <adrianpearman12@gmail.com>
2017-03-28 17:06:00 -0400

    update exercise 6

--- exercise10.rb
+++ exercise10.rb
@@ -1,21 +1,28 @@
 # 1 - Start out by creating the following hash representing the number of students in past Bitmaker cohorts:
 students = {:cohort1 => 34, :cohort2 => 42, :cohort3 => 22}
 # 2 - Create a method that displays the name and number of students for each cohort on seperate lines
-puts "#{students}"
+students.each { |key, value| puts "#{key}: #{value} students" }
 
 # 3 - Add cohort 4, which had 43 students, to the hash.
 students[:cohort4] = 43
 puts "#{students}"
 
+
 # 4 - Use the keys method to output all of the cohort names.
+students.each { |key, value| puts "#{key}: #{value} students" }
 
 
 # 5 -The classrooms have been expanded! Increase each cohort size by 5% and display the new results.
+#students_update = students[students.map {|key, value| [key, value * 1.05]}]
+#students_update.each { |key, value| puts "#{key}: #{value} students" }
+
 
 
 # 6 - Delete the 2nd cohort and redisplay the hash.
-students.delete(:cohort2)
 puts "#{students}"
+students.delete(:cohort2)
+students.each { |key, value| puts "#{key}: #{value} students" }
+
 
 
 
--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,11 @@
-puts "What is the Temperature in Fahrenheit"
-temperature = gets.to_f
-convert = ((temperature - 32) * 5)/9
-puts "The Temperatue in Celscius is #{convert.to_s}"
+puts "What is the Temperature in Fahrenheit??"
+temperaturef = gets.to_f
+convertc = ((temperaturef - 32) * 5)/9
+puts "The Temperatue in Celscius is #{convertc.to_s}"
+
+puts "What is the Temperatue in Celscius??"
+temperaturec = gets.to_f
+convertf = ((temperaturec * 9) / 5)+32
+puts "The Temperatue in Fahrenheit is #{convertf.to_s}"
+
+#Multiply by 9, then divide by 5, then add 32
