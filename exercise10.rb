24f6a141cc5ad991f76e36b9adb3f7f835e2ebda
Adrian Pearman <adrianpearman12@gmail.com>
2017-03-28 16:06:09 -0400

    began exercise 10

---
+++ exercise10.rb
@@ -0,0 +1,24 @@
+# 1 - Start out by creating the following hash representing the number of students in past Bitmaker cohorts:
+students = {:cohort1 => 34, :cohort2 => 42, :cohort3 => 22}
+# 2 - Create a method that displays the name and number of students for each cohort on seperate lines
+puts "#{students}"
+
+# 3 - Add cohort 4, which had 43 students, to the hash.
+students[:cohort4] = 43
+puts "#{students}"
+
+# 4 - Use the keys method to output all of the cohort names.
+
+
+# 5 -The classrooms have been expanded! Increase each cohort size by 5% and display the new results.
+
+
+# 6 - Delete the 2nd cohort and redisplay the hash.
+students.delete(:cohort2)
+puts "#{students}"
+
+
+
+# 7 - BONUS: Calculate the total number of students across all cohorts using each and a variable to keep track of the total. Output the result.
+
+# 8 - BONUS: Create another similar hash called staff and display it using the same method.
